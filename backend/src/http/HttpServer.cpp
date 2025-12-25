5bcfa81f5086e05a03ba76c1dca85cdea11b0eab
h24021 <hamnahassan@example.com>
2025-12-25 22:50:46 +0500

    Fix CORS headers for cross-origin requests

--- backend/src/http/HttpServer.cpp
+++ backend/src/http/HttpServer.cpp
@@ -234,4 +234,7 @@ string HttpServer::urlDecode(const string& s) {
 
 void HttpServer::addCorsHeaders(HttpResponse& res) {
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Max-Age", "86400");
 }
